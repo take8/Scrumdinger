04f03053124b2b896b98f6c68a8814d1c1554a14
Takeya <stk.x.100@gmail.com>
2022-07-25 03:23:16 +0900

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Takeya Shimizu on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
